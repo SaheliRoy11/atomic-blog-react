06c2910148390622154b67b5ef0e2fd07d93c5ba
Saheli <engr.saheliroy@gmail.com>
2025-09-08 21:50:11 +0530

    useMemo hook to memoize objects passed as props to memoized components

--- src/App-memo.js
+++ src/App-memo.js
@@ -1,4 +1,4 @@
-import { memo, useEffect, useState } from "react";
+import { memo, useEffect, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -42,6 +42,16 @@ function App() {
     [isFakeDark]
   );
 
+  //useMemo takes in a callback function, which is called on initial render, and a dependency array.The result returned from the function will be stored in cache.The dependency array determines when the calculations (callback function) will be executed again, just like useEffect hook.
+  //[] = calculated at initial render and should never change, not re-computed
+  const archiveOptions = useMemo(() => {
+    //There can be other intensive calculations
+    return {
+    show: false,
+    title: `Posts archive in addition to ${posts.length} main posts`
+  };
+  }, [posts.length]) 
+
   return (
     <section>
       <button
@@ -58,7 +68,7 @@ function App() {
         setSearchQuery={setSearchQuery}
       />
       <Main posts={searchedPosts} onAddPost={handleAddPost} />
-      <Archive show={false} />
+      <Archive  archiveOptions={archiveOptions}/>
       <Footer />
     </section>
   );
@@ -155,18 +165,18 @@ function List({ posts }) {
   );
 }
 
-const Archive = memo(function Archive({ show }) {
+const Archive = memo(function Archive({ archiveOptions }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
     Array.from({ length: 10000 }, () => createRandomPost())
   );
 
-  const [showArchive, setShowArchive] = useState(show);
+  const [showArchive, setShowArchive] = useState(archiveOptions.show);
 
   return (
     <aside>
-      <h2>Post archive</h2>
+      <h2>{archiveOptions.title}</h2>
       <button onClick={() => setShowArchive((s) => !s)}>
         {showArchive ? "Hide archive posts" : "Show archive posts"}
       </button>
