e25b64a376d482018a48d1368dd7326514442445
Saheli <engr.saheliroy@gmail.com>
2025-09-13 23:25:43 +0530

    Optimize context re-renders by memoizing context value

--- src/PostContext.js
+++ src/PostContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -36,16 +36,20 @@ function PostProvider({ children }) {
     setPosts([]);
   }
 
-  return (
-    //2)PROVIDE VALUE TO CHILD COMPONENTS
-    <PostContext.Provider
-      value={{
+  const value = useMemo(() => {
+    return {
         posts: searchedPosts,
         onAddPost: handleAddPost,
         onClearPosts: handleClearPosts,
         searchQuery,
         setSearchQuery,
-      }}
+      }
+}, [searchedPosts, searchQuery]);
+
+  return (
+    //2)PROVIDE VALUE TO CHILD COMPONENTS
+    <PostContext.Provider
+      value={value}
     >
       {children}
     </PostContext.Provider>
